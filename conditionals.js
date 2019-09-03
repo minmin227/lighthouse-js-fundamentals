4435e4b7c26800180f8d853b3211b921a6af3c97
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-09-03 02:16:15 +0000

    note about the weather

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temp = 12;
+
+if (temp < 0){
+console.log('pick out a scarf');
+} else if (temp < 15) {
+console.log('wont cut it');
+} else {
+console.log('are fine')
+}
+
+console.log('now you re ready to go outside');
